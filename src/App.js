dbc0b18e8435dc7b5177dfe3234639f82d242e2d
Zeenat <zeenatlawal82@gmail.com>
2024-03-09 12:57:19 +0100

    Override MuiTextField styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,39 @@ import "./App.css";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { BookOwner } from "./Pages/BookOwner";
-// import { Header } from "./components/Header";
 
 const theme = createTheme({
   typography: {
     fontFamily: "Poppins, sans-serif",
     allVariants: {
       color: "#fff",
+    },
+  },
+  components: {
+    MuiTextField: {
+      defaultProps: {
+        variant: "outlined",
+        size: "small",
+      },
+      styleOverrides: {
+        root: {
+          "& .MuiOutlinedInput-root": {
+            backgroundColor: "#131030CF",
+            "& fieldset": {
+              borderColor: "#5256A9A6",
+            },
+            "&:hover fieldset": {
+              borderColor: "#5256A9A6",
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "#5256A9A6",
+            },
+          },
+          "& .MuiOutlinedInput-input": {
+            color: "#7c88a3",
+          },
+        },
+      },
     },
   },
 });
