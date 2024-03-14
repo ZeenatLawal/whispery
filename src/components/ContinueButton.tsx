6bb8c67c195d2de8588127d695d982971a607c50
Zeenat <zeenatlawal82@gmail.com>
2024-03-14 22:37:02 +0100

    Create theme carousel and modify continue button style

--- src/components/ContinueButton.tsx
+++ src/components/ContinueButton.tsx
@@ -2,19 +2,20 @@ import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 
-export function ContinueButton({ path }:{path:string}) {
+export function ContinueButton({ path }: { path: string }) {
   const navigate = useNavigate();
 
   return (
     <Button
       variant="contained"
       sx={{
+        width: "188px",
         height: "50px",
         borderRadius: "36px",
         fontWeight: 600,
-        padding: "10px 30px",
         background: "linear-gradient(90deg, #718DF4 14.67%, #ED65F3 100%)",
         marginBottom: "30px",
+        boxShadow: "0px 0px 44.599998474121094px 0px rgba(143, 0, 255, 0.51)",
       }}
       endIcon={<ArrowRightAltIcon fontSize="large" />}
       onClick={() => {
