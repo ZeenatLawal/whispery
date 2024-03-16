540e459394dd1312210e0790dee3024a99335b69
Zeenat <zeenatlawal82@gmail.com>
2024-03-16 14:56:11 +0100

    Remove fixed bgColor

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { BookOwner } from "./Pages/BookOwner";
