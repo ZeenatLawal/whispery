e89b9f40f12b12742be05e707e825d2028b47f91
Zeenat <zeenatlawal82@gmail.com>
2024-04-01 08:01:13 +0100

    update fetch function

--- src/hooks/apiFetch.ts
+++ src/hooks/apiFetch.ts
@@ -7,26 +7,22 @@ export async function apiFetch<T>({
   data?: any;
   method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
 }) {
-
   const baseUrl = "https://darrol-backend.onrender.com/v1";
 
   return fetch(`${baseUrl}${path}`, {
-      method, // *GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors, *cors, same-origin
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "same-origin", // include, *same-origin, omit
-      headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-      },
-      redirect: "follow", // manual, *follow, error
-      body: method !== "GET" ? JSON.stringify(data) : undefined,
+    method,
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: method !== "GET" ? JSON.stringify(data) : undefined,
   })
-      .then(async (e) => {
-          if (!e.ok) {
-              return Promise.reject(await e.json());
-          }
-          return e.json();
-      })
-      .then((e) => Promise.resolve<T>(e));
+    .then(async (e) => {
+      if (!e.ok) {
+        return Promise.reject(await e.json());
+      }
+      return e.json();
+    })
+    .then((e) => Promise.resolve<T>(e));
 }
