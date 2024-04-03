1829ecef96a6f0efce8c4db613d3c00b41c9b3e8
Zeenat <zeenatlawal82@gmail.com>
2024-04-03 15:11:42 +0100

    Change page 3 text to Dutch

--- src/Pages/PersonalEvents.tsx
+++ src/Pages/PersonalEvents.tsx
@@ -1,4 +1,13 @@
-import { Grid, Button, TextField, useMediaQuery } from "@mui/material";
+import {
+  Grid,
+  Button,
+  TextField,
+  InputLabel,
+  useMediaQuery,
+  List,
+  ListItem,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
 import { Header } from "../components/Header";
 import { ContinueButton } from "../components/ContinueButton";
@@ -9,11 +18,15 @@ import { PageTitle } from "../components/PageTitle";
 export function PersonalEvents() {
   const isSmallest = useMediaQuery("(max-width: 380px)");
   const [personalEvent, setPersonalEvent] = useState("");
+  const [allEvents, setAllEvents] = useState<string[]>([]);
 
   return (
     <Header pageNumber={3} path="/members">
       <Grid item xs={12} textAlign="center">
-        <PageTitle title="Add personal events" />
+        <PageTitle
+          title="Voeg persoonlijke gebeurtenissen toe"
+          subtitle="Deze persoonlijke gebeurtenissen maken straks onderdeel uit van het verhaal."
+        />
         <Button
           variant="outlined"
           sx={{
@@ -27,49 +40,112 @@ export function PersonalEvents() {
           }}
           startIcon={<InfoOutlinedIcon fontSize="large" />}
         >
-          Maximum 3
+          Maximaal 3
         </Button>
-        <Grid item>
+      </Grid>
+
+      <Grid
+        item
+        container
+        xs={12}
+        marginTop="25px"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Grid
+          item
+          xs={12}
+          md={6}
+          style={{
+            padding: "40px 25px",
+            backgroundColor: "#17142B",
+            borderRadius: "15px",
+            boxShadow: "inset 2px 2px 0 0 #8B7CF8, inset 0 0 2px 2px #ED65F3",
+          }}
+        >
+          <InputLabel htmlFor="add-event">
+            Gebeurtenis {allEvents.length + 1}
+          </InputLabel>
           <TextField
-            sx={{
-              margin: { xs: "65px 0", md: "60px 0" },
-              width: isSmallest ? "300px" : { xs: "380px", md: "447px" },
-            }}
-            placeholder="ex: Visit to the zoo"
+            id="add-event"
+            placeholder="Bijv. Het gezin krijgt binnenkort een puppy"
+            fullWidth
             value={personalEvent}
             onChange={(e) => setPersonalEvent(e.target.value)}
           />
         </Grid>
-        <Button
-          variant="outlined"
-          sx={{
-            borderRadius: "31px",
-            boxShadow: "inset 1px 1px 0 0 #8B7CF8, inset 0 0 1px 1px #ED65F3",
-            width: "248px",
-            height: "50px",
-          }}
-          startIcon={<AddIcon sx={{ color: "#C665F3" }} fontSize="large" />}
-        >
+
+        <Grid item xs={12} textAlign="center" marginTop="16px">
+          <Button
+            variant="outlined"
+            sx={{
+              borderRadius: "31px",
+              boxShadow: "inset 1px 1px 0 0 #8B7CF8, inset 0 0 1px 1px #ED65F3",
+              width: isSmallest ? "300px" : { xs: "380px", md: "320px" },
+              height: "50px",
+              "&:disabled": { opacity: "0.4" },
+            }}
+            startIcon={<AddIcon sx={{ color: "#C665F3" }} fontSize="large" />}
+            onClick={() => {
+              setAllEvents((prevEvent) => {
+                if (prevEvent.includes(personalEvent)) {
+                  return prevEvent;
+                } else {
+                  return [...prevEvent, personalEvent];
+                }
+              });
+              setPersonalEvent("");
+            }}
+            disabled={allEvents.length === 3}
+          >
+            <div
+              style={{
+                backgroundImage:
+                  "linear-gradient(90deg, #8B7CF8 0%, #ED65F3 85.48%)",
+                backgroundClip: "text",
+                color: "transparent",
+                fontSize: "16px",
+              }}
+            >
+              Voeg nog een gebeurtenis toe
+            </div>
+          </Button>
+        </Grid>
+
+        {allEvents.length > 0 && (
           <div
             style={{
-              backgroundImage:
-                "linear-gradient(90deg, #8B7CF8 0%, #ED65F3 85.48%)",
-              backgroundClip: "text",
-              color: "transparent",
-              fontSize: "16px",
+              padding: "25px 40px",
+              marginTop: "16px",
+              backgroundColor: "#17142B",
+              borderRadius: "15px",
+              boxShadow: "inset 2px 2px 0 0 #8B7CF8, inset 0 0 2px 2px #ED65F3",
             }}
           >
-            Add an Event
+            <List component="ol">
+              {allEvents.map((event, index) => (
+                <ListItem key={index} component="li">
+                  <Typography>
+                    <strong>{index + 1}. </strong> {event}
+                  </Typography>
+                </ListItem>
+              ))}
+            </List>
           </div>
-        </Button>
+        )}
       </Grid>
 
       <Grid
         container
         justifyContent="center"
-        marginTop={{ xs: "338px", md: "340px" }}
+        marginTop={{ xs: "118px", md: "139.5px" }}
       >
-        <ContinueButton path="/themes" />
+        <ContinueButton
+          path="/themes"
+          onClick={async () => {
+            console.log("send event details");
+          }}
+        />
       </Grid>
     </Header>
   );
