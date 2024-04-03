b51701a150f0c81e8ac3ddada58ae9afb911d5a7
Zeenat <zeenatlawal82@gmail.com>
2024-04-03 12:00:08 +0100

    Add subtitle to page title

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,12 +1,21 @@
 import { Typography } from "@mui/material";
 
-export function PageTitle({ title }: { title: string }) {
+export function PageTitle({
+  title,
+  subtitle,
+}: {
+  title: string;
+  subtitle?: string;
+}) {
   return (
-    <Typography
-      style={{ fontWeight: 700 }}
-      fontSize={{ xs: "28px", md: "40px" }}
-    >
-      {title}
-    </Typography>
+    <>
+      <Typography
+        style={{ fontWeight: 700 }}
+        fontSize={{ xs: "28px", md: "40px" }}
+      >
+        {title}
+      </Typography>
+      <Typography variant="subtitle1">{subtitle}</Typography>
+    </>
   );
 }
