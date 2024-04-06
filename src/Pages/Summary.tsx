188fd52659f1b295e0082fd3fd21faf2f1607d75
Zeenat <zeenatlawal82@gmail.com>
2024-04-06 12:26:12 +0100

    Change page 6 text to Dutch

--- src/Pages/Summary.tsx
+++ src/Pages/Summary.tsx
@@ -1,17 +1,32 @@
-import { Grid, useTheme, useMediaQuery, Typography } from "@mui/material";
+import {
+  Grid,
+  useTheme,
+  useMediaQuery,
+  Typography,
+  List,
+  ListItem,
+  Button,
+} from "@mui/material";
 import { Header } from "../components/Header";
-import { ContinueButton } from "../components/ContinueButton";
 import { PageTitle } from "../components/PageTitle";
+import { useNavigate } from "react-router-dom";
 
 export function Summary() {
   const theme = useTheme();
+  const navigate = useNavigate();
   const isSmallest = useMediaQuery("(max-width: 380px)");
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
+
+  const textStyle = {
+    color: "#D1BBED",
+    fontWeight: 500,
+    fontSize: "18px",
+  };
 
   return (
     <Header pageNumber={6} path="/message">
       <Grid item xs={12} textAlign="center">
-        <PageTitle title="Wow, that would be a great book!" />
+        <PageTitle title="Wauw, dat wordt een gaaf boek!" />
       </Grid>
 
       <Grid
@@ -42,35 +57,51 @@ export function Summary() {
               marginBottom: "47px",
             }}
           >
-            Short summary:
+            Nog even ter controle:
           </Typography>
 
-          <Typography
+          <div
             style={{
-              color: "#D1BBED",
               overflow: "auto",
-              fontWeight: 500,
               textAlign: "center",
-              fontSize: "18px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-            arcu erat, elementum vitae laoreet finibus, sagittis vel leo. Nam
-            facilisis magna ligula, ut blandit ipsum ornare id. Integer ornare
-            enim a massa elementum malesuada.
-            <br />
-            <br />
-            Vivamus augue ipsum, hendrerit et neque sed, tincidunt dapibus ante.
-            Nulla vitae ante volutpat ex dictum eleifend volutpat in velit. Ut
-            non enim vel orci condimentum blandit sit amet sed nunc. Donec
-            tincidunt vehicula finibus.
-            <br />
-            <br />
-            Vivamus augue ipsum, hendrerit et neque sed, tincidunt dapibus ante.
-            Nulla vitae ante volutpat ex dictum eleifend volutpat in velit. Ut
-            non enim vel orci condimentum blandit sit amet sed nunc. Donec
-            tincidunt vehicula finibus.
-          </Typography>
+            <Typography style={textStyle}>
+              Als ik het goed begrijp wil je een boek laten maken voor [NAME],
+              een [GENDER] van [AGE]. Het gezin bestaat uit [FAMILY MEMBER 1],
+              [FAMILY MEMBER 2] en [FAMILY MEMBER 3]. Het boek krijgt een
+              [THEME] thema waarin de volgende persoonlijke gebeurtenissen
+              worden meegenomen:
+            </Typography>
+
+            <List>
+              <ListItem>
+                <Typography style={textStyle}>
+                  <strong>1. </strong> [PERSONAL EVENT 1]
+                </Typography>
+              </ListItem>
+
+              <ListItem>
+                <Typography style={textStyle}>
+                  <strong>2. </strong> [PERSONAL EVENT 2]
+                </Typography>
+              </ListItem>
+
+              <ListItem>
+                <Typography style={textStyle}>
+                  <strong>3. </strong> [PERSONAL EVENT 3]
+                </Typography>
+              </ListItem>
+            </List>
+
+            <Typography style={textStyle}>
+              Klopt dit niet? Ga dan terug naar de vorige stappen en pas de
+              gegevens aan.
+            </Typography>
+          </div>
         </div>
       </Grid>
 
@@ -79,12 +110,25 @@ export function Summary() {
         justifyContent="center"
         marginTop={{ xs: "65px", md: "70px" }}
       >
-        <ContinueButton
-          path="/userDetails"
+        <Button
+          variant="contained"
+          sx={{
+            width: "288px",
+            height: "50px",
+            borderRadius: "36px",
+            fontWeight: 600,
+            fontSize: "18px",
+            background: "linear-gradient(90deg, #718DF4 14.67%, #ED65F3 100%)",
+            marginBottom: "30px",
+            boxShadow: "0px 0px 44.6px 0px #8F00FF82",
+          }}
           onClick={async () => {
             console.log("send summary");
+            navigate("/userDetails");
           }}
-        />
+        >
+          De gegevens kloppen
+        </Button>
       </Grid>
     </Header>
   );
